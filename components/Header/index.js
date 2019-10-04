c4fd9636c82066772c9df2497ab42d4ecae904b5
DeAndre Johnson <dre@phreshr.com>
2019-10-04 23:33:21 +0800

    added header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,3 +1,4 @@
+import createElement from '../../lib/createElement.js'
 // STEP 1: Create a header component.
 // -----------------------
 // Using a function create the component you see below:
@@ -9,4 +10,17 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const { div, h1, span } = createElement()
+
+function Header() {
+  return div({ class: "header" }, [
+    span({ class: 'date' }, 'SMARCH 28, 2019'),
+    h1({}, 'Lambda Times'),
+    span({ class: 'temp' }, '98')
+  ])
+}
+
+const h = Header()
+
+const container = document.querySelector('.header-container')
+container.appendChild(h)
