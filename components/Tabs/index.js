7dce7f13a48ecc355b26ece79efbadd7ed88d573
DeAndre Johnson <dre@phreshr.com>
2019-10-04 23:44:02 +0800

    added topics

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,3 +1,5 @@
+import createElement from '../../lib/createElement.js'
+
 // Step 2: Create Tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
@@ -7,3 +9,22 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const container = document.querySelector('.topics')
+
+const { div } = createElement()
+
+axios
+  .get('https://lambda-times-backend.herokuapp.com/topics')
+  .then(response => {
+    const topics = response.data.topics
+    topics.forEach(topic => {
+      const t = Tab(topic)
+      container.appendChild(t)
+    });
+  })
+
+function Tab(topic) {
+  return div({ class: "tab" }, topic)
+}
+
